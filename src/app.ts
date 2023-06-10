5f56b1af8f79d5e4b783aa70c9b04cb506c89efd
Kacper Smietana <kabox9@gmail.com>
2023-06-10 12:26:57 +0200

    demo with cart

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,93 @@ import './style.scss';
 
 const closeMenuBtn = document.querySelector('.close-menu')!;
 const mobileMenu = document.querySelector('.mobile-menu')!;
-const burgerBtn = document.querySelector('.burger')
+const burgerBtn = document.querySelector('.burger')!;
+
+const bigPicture = document.querySelector('.big-image-img')!;
 const body = document.querySelector('body')!;
 
-console.log(mobileMenu);
-console.log(closeMenuBtn);
+const cartBtns = document.querySelectorAll('.cart')!
+const cart = document.querySelector('.cart-display')! as HTMLDivElement
+
+const addToCartBtn = document.querySelector('.add')!;
+const plus = document.querySelector('.plus')!;
+const minus = document.querySelector('.minus')!;
+const pairAmount = document.querySelector('.item-amount')!
+let amount = 0;
+
+
+const smallPicBoxes = document.querySelectorAll('.slider-image')
+const smallPics = document.querySelectorAll('.small-picture');
+
+const imageValues = {
+    firstImg: {
+        path: '../src/images/image-product-1.jpg'
+    },
+    secondImg: {
+        path: '../src/images/image-product-2.jpg'
+    },
+    thirdImg: {
+        path: '../src/images/image-product-3.jpg'
+    },
+    fourthImg: {
+        path: '../src/images/image-product-4.jpg'
+    }
+}
+
+
+// clears active states on slider images
+const clearActiveStates = () => {
+    for (const box of smallPicBoxes) {
+        box.classList.remove('slider-image--active')
+    }
+}
+
+const handlePickChange = (pic: Element, path: string) => {
+    const box = pic.closest('.slider-image')
+    clearActiveStates()
+    box?.classList.add('slider-image--active')
+    bigPicture.setAttribute('src', path)
+}
+
+for (const pic of smallPics) {
+    pic.addEventListener('click', () => {
+        if (pic.classList.contains('small-picture-first')) {
+            handlePickChange(pic, imageValues.firstImg.path)
+        }
+        if (pic.classList.contains('small-picture-second')) {
+            handlePickChange(pic, imageValues.secondImg.path)
+        }
+        if (pic.classList.contains('small-picture-third')) {
+            handlePickChange(pic, imageValues.thirdImg.path)
+        }
+        if (pic.classList.contains('small-picture-fourth')) {
+            handlePickChange(pic, imageValues.fourthImg.path)
+        }
+    })
+}
+
+// shows current amount of items
+const handleAmount = () => {
+    pairAmount.textContent = amount.toString();
+}
+
+// increases amount of items to add to cart
+const increaseAmount = () => {
+    if (amount < 10) {
+        amount++;
+        handleAmount()
+    }
+}
+
+// decreases amount of items to add to cart
+const decreaseAmount = () => {
+    if (amount > 0) {
+        amount--;
+        handleAmount()
+    }
+}
+
+
 
 burgerBtn?.addEventListener('click', () => {
     mobileMenu.classList.add('mobile-menu--active')
@@ -19,3 +101,18 @@ closeMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('mobile-menu--active')
     body.style.overflow = 'auto'
 })
+
+for (const cartBtn of cartBtns) {
+    cartBtn.addEventListener('click', () => {
+        if ((cart as HTMLDivElement).style.display == 'flex') {
+            (cart as HTMLDivElement).style.display = 'none'
+        } else {
+            (cart as HTMLDivElement).style.display = 'flex'
+        }
+    })
+}
+
+plus?.addEventListener('click', increaseAmount)
+minus?.addEventListener('click', decreaseAmount)
+
+handleAmount();
