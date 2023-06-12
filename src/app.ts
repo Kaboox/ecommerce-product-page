303053034b1236592f151d5737b09b2e263f40de
Kacper Smietana <kabox9@gmail.com>
2023-06-12 20:38:21 +0200

    pic paths

--- src/app.ts
+++ src/app.ts
@@ -56,7 +56,7 @@ const imageValues = {
 const imgPaths:string[] = ["src/images/image-product-1.jpg",
 "src/images/image-product-2.jpg",
 "src/images/image-product-3.jpg",
-"/src/images/image-product-4.jpg"];
+"src/images/image-product-4.jpg"];
 
 
 // clears active states on slider images
